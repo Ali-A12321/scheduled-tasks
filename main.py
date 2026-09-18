5186fa9282c0430a6a8dd38209849afc9adf1fad
Ali-A12321 <alihassanamin2012@gmail.com>
2026-09-18 22:42:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,72 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
+import smtplib as sm
+import datetime as dt
+import random as rd
+import pandas as pd
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+my_email = "pythonautomation74@gmail.com"
+my_password = os.environ.get("SMTP_PASSWORD")
+monday_emails = ["alihassanamin2012@gmail.com","23a.amin@preston-manor.com","mrhadiamin2007@gmail.com","fatimalissa@yahoo.co.uk","23f.burhan@preston-manor.com","samaatiah54@gmail.com"]
+now = dt.datetime.now()
+if now.weekday() == 0:
+    with open("quotes.txt") as file:
+        lines = file.readlines()
+        quote = rd.choice(lines)
+        with sm.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=my_password)
+            for email in monday_emails:
+                connection.sendmail(from_addr=my_email, to_addrs=email, msg=f"Subject:Monday Motivation!\n\n{quote}\nFrom Ali Amin")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+today = (now.month, now.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+data = pd.read_csv("birthdays.csv")
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+if today in birthday_dict:
+    with open(f"letter_templates/letter_{rd.randint(1,3)}.txt") as file:
+        body = file.read()
+        body = body.replace("[NAME]", birthday_dict[today]["name"])
+    with sm.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email, to_addrs=birthday_dict[today]["email"], msg=f"Subject:Happy Birthday!\n\n{body}")
+
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY")
+with open("rain_emails.txt") as file:
+    emails = file.readlines()
+MY_LAT = 51.564072
+MY_LNG = -0.295402
+weather_parameters = {
+    "lat":MY_LAT,
+    "lon":MY_LNG,
+    "appid":api_key,
+    "cnt":4
+}
+response = requests.get(OWM_endpoint, params=weather_parameters)
+response.raise_for_status()
+weather_data = response.json()
+will_rain = False
+idx=0
+rain_indexes=[]
+for dict in weather_data["list"]:
+    if dict["weather"][0]["id"] < 700:
+        will_rain = True
+        rain_indexes.append(f"{idx}-{idx+3}hrs")
+    idx += 3
+rain_str = ", ".join(rain_indexes)
+rain_str2 = "\n".join(rain_indexes)
+
+if will_rain:
+    with sm.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        for email in emails:
+            connection.sendmail(
+            from_addr=my_email,
+            to_addrs=email,
+            msg=f"""Subject:{rain_str}: It is going to rain!\n\n
+                It is going to rain at within:\n{rain_str2}.\nBring an umbrella!"""
         )
